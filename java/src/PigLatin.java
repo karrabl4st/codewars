0e89f8fddb6e36ee2e87f1a9e8ce9b5d33e2df93
karrabl4st <karraisepic@gmail.com>
2021-09-14 15:45:31 -0500

    turn java folder into java project

---
+++ java/src/PigLatin.java
@@ -0,0 +1,14 @@
+public class PigLatin {
+    public static String pigIt(String str) {
+      
+      String newStr = "";
+      for (int i = 0; i < str.split(" |!").length; i++) {
+        
+        
+        
+      }
+
+      return newStr;
+      
+    }
+}
