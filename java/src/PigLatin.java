95b18178f90048b8d43391037a3d997d4d6c487b
karrabl4st <karraisepic@gmail.com>
2021-09-16 20:38:52 -0500

    change

--- java/src/PigLatin.java
+++ java/src/PigLatin.java
@@ -2,11 +2,19 @@ public class PigLatin {
     public static String pigIt(String str) {
       
       String newStr = "";
-      for (int i = 0; i < str.split(" |!").length; i++) {
+      String[] strArr = str.split(" &[^!.?,]");
+
+      for (int i = 0; i < strArr.length - 1; i++) {
         
-        
+        newStr += strArr[i].substring(1);
+        newStr += strArr[i].substring(0, 1);
+
+        newStr += " ";
         
       }
+
+      newStr += strArr[strArr.length - 1].substring(1);
+      newStr += strArr[strArr.length - 1].substring(0, 1);
 
       return newStr;
       
--- java/tst/Tests.java
+++ java/tst/Tests.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class Tests {
     @Test
-    public void FixedTests() {
+    public void codeTests() {
         assertEquals("igPay atinlay siay oolcay", PigLatin.pigIt("Pig latin is cool"));
         assertEquals("hisTay siay ymay tringsay", PigLatin.pigIt("This is my string"));
     }
