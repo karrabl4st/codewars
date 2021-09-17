259028363dc6d1a1fa913e718b93e646a31a01b1
karrabl4st <karraisepic@gmail.com>
2021-09-17 15:45:39 -0500

    change

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Nice day out, huh?";
-        str = str.replaceAll("[,!?.]+", "");
-        System.out.println(PigLatin.pigIt("Pig latin is cool!"));
-        for (String seg : str.split(" ")) {
+        String str = "Nice day out, huh[ ]]";
+        //System.out.println(PigLatin.pigIt("Pig latin is cool[ ]!"));
+        for (String seg : str.split("\\w[^ ]+")) {
             System.out.println(seg);
         }
 
--- java/src/PigLatin.java
+++ java/src/PigLatin.java
@@ -2,15 +2,14 @@ public class PigLatin {
     public static String pigIt(String str) {
       
       String newStr = "";
-      String strExclude = str.replaceAll("[,!?.]+", "");
-      String[] strArr = str.split(" ");
-      String[] strExcludeArr = strExclude.split(" ");
+      String[] strArr = str.split("\\w[^ ]");
+      String[] strExcludeArr = str.split("[^\\w]+");
 
       for (int i = 0; i < strExcludeArr.length - 1; i++) {
         
         newStr += strExcludeArr[i].substring(1);
         newStr += strExcludeArr[i].substring(0, 1) + "ay";
-        newStr += strArr[i].replaceAll("[^,!?.]", "");
+        newStr += strArr[i].replaceAll("[^,!?.\\[\\]]", "");
 
         newStr += " ";
         
@@ -18,7 +17,7 @@ public class PigLatin {
 
       newStr += strExcludeArr[strExcludeArr.length - 1].substring(1);
       newStr += strExcludeArr[strExcludeArr.length - 1].substring(0, 1) + "ay";
-      newStr += strArr[strArr.length - 1].replaceAll("[^,!?.]", "");
+      newStr += strArr[strArr.length - 1].replaceAll("[^,!?.\\[\\]]", "");
 
       return newStr;
       
