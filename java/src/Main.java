5383cf3f06286566cef7bf646ab1a0bada89a0a1
karrabl4st <karraisepic@gmail.com>
2021-09-17 22:10:01 -0500

    changes

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,11 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Nice day out, huh[ ]]";
-        //System.out.println(PigLatin.pigIt("Pig latin is cool[ ]!"));
-        for (String seg : str.split("\\w[^ ]+")) {
-            System.out.println(seg);
-        }
+        System.out.println(PigLatin.pigIt("Pig [latin is cool[ ]]"));
 
     }
     
--- java/src/PigLatin.java
+++ java/src/PigLatin.java
@@ -1,23 +1,35 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PigLatin {
     public static String pigIt(String str) {
+
+      final Pattern pattern = Pattern.compile("\\W+");
+      final Matcher matcher = pattern.matcher(str);
+
+      ArrayList<String> list = new ArrayList<>();
+
+      while (matcher.find()) {
+        for (int i = 0; i <= matcher.groupCount(); i++) {
+          list.add(matcher.group(i));
+        }
+      }
       
       String newStr = "";
-      String[] strArr = str.split("\\w[^ ]");
-      String[] strExcludeArr = str.split("[^\\w]+");
+      final String[] strArr = str.split("\\W+");
 
-      for (int i = 0; i < strExcludeArr.length - 1; i++) {
+      for (int i = 0; i < strArr.length - 1; i++) {
         
-        newStr += strExcludeArr[i].substring(1);
-        newStr += strExcludeArr[i].substring(0, 1) + "ay";
-        newStr += strArr[i].replaceAll("[^,!?.\\[\\]]", "");
-
-        newStr += " ";
+        newStr += strArr[i].substring(1);
+        newStr += strArr[i].substring(0, 1) + "ay";
+        newStr += list.get(i);
         
       }
 
-      newStr += strExcludeArr[strExcludeArr.length - 1].substring(1);
-      newStr += strExcludeArr[strExcludeArr.length - 1].substring(0, 1) + "ay";
-      newStr += strArr[strArr.length - 1].replaceAll("[^,!?.\\[\\]]", "");
+      newStr += strArr[strArr.length - 1].substring(1);
+      newStr += strArr[strArr.length - 1].substring(0, 1) + "ay";
+      newStr += list.get(list.size() - 1);
 
       return newStr;
       
